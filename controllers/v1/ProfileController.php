67afefb98f791d3bbed4245d4de122d708695dfc
Abdilah Sammi <43635222+samdgea@users.noreply.github.com>
2019-12-07 16:10:55 +0700

    Add Edit Profile Feature
    Menambahkan fitur edit account profile

--- controllers/v1/ProfileController.php
+++ controllers/v1/ProfileController.php
@@ -7,6 +7,8 @@ use yii\rest\Controller;
 use app\models\User;
 use app\models\AccessToken;
 
+use app\models\Forms\EditAccountForm;
+
 use yii\filters\auth\HttpBearerAuth;
 
 class ProfileController extends Controller 
@@ -15,6 +17,9 @@ class ProfileController extends Controller
      * @var string the model class name. This property must be set.
      */
     public $modelClass = User::class;
+
+    protected $_response;
+    protected $_code = 500;
 
     /**
      * {@inheritdoc}
@@ -29,57 +34,6 @@ class ProfileController extends Controller
         return $behaviors;
     }
 
-    public function actionMe() 
-    {
-        if (Yii::$app->request->isGet) {
-            $user = Yii::$app->user->identity;
-
-            unset($user['password_hashed']);
-
-            return Yii::createObject([
-                'class' => 'yii\web\Response',
-                'format' => \yii\web\Response::FORMAT_JSON,
-                'data' => [
-                    'success' => true,
-                    'desc' => 'Detail Profil',
-                    'data' => $user
-                ],
-                'statusCode' => 200
-            ]);
-        } else {
-            return Yii::$app->user->identity->getAuthKey();
-            // return Yii::createObject([
-            //     'class' => 'yii\web\Response',
-            //     'format' => \yii\web\Response::FORMAT_JSON,
-            //     'data' => [
-                    
-            //     ],
-            //     'statusCode' => 200
-            // ]);
-        }
-    }
-
-    public function actionLogout() 
-    {
-        $headers = Yii::$app->request->getHeaders();
-        $headers = explode(" ", $headers['authorization']);
-
-        $accessToken = AccessToken::find()->where(['access_token' => $headers[1]])->one();
-        $accessToken->token_valid = false;
-        $accessToken->save();
-
-        return Yii::createObject([
-            'class' => 'yii\web\Response',
-            'format' => \yii\web\Response::FORMAT_JSON,
-            'data' => [
-                'success' => true,
-                'desc' => 'Berhasil keluar',
-                'data' => null
-            ],
-            'statusCode' => 200
-        ]);
-    }
-
     /**
      * Declares the allowed HTTP verbs.
      * Please refer to [[VerbFilter::actions]] on how to declare the allowed verbs.
@@ -91,5 +45,87 @@ class ProfileController extends Controller
             'logout' => ['POST'],
             'me' => ['GET', 'POST']
         ];
+    }
+
+    /**
+     * Profile Details
+     * @method GET = Retrieve user profile details
+     * @method POST = Modify user profile details
+     * 
+     * @return \yii\BaseYii|object
+     */
+    public function actionMe() 
+    {
+        if (Yii::$app->request->isGet) {
+            $user = Yii::$app->user->identity;
+
+            unset($user['password_hashed']);
+
+            $this->_response = [
+                'success' => true,
+                'desc' => 'Detail Profil',
+                'data' => $user
+            ];
+
+            $this->_code = 200;
+        } else {
+            $model = new EditAccountForm();
+
+            if ($model->load(Yii::$app->request->post(), '') && $model->executeEditAccount())
+            {
+                $this->_response = [
+                    'success' => true,
+                    'desc' => 'Berhasil mengubah detail profil',
+                    'data' => $model->getUser()
+                ];
+
+                $this->_code = 200;
+            } else {
+                $this->_response = [
+                    'success' => false,
+                    'desc' => 'Permintaan anda tidak sesuai dengan validasi',
+                    'data' => $model->getErrorSummary($model->getErrors())
+                ];
+
+                $this->_code = 400;
+            }
+        }
+
+        return $this->_sendResponse($this->_response, $this->_code);
+    }
+
+    /**
+     * Logout session
+     * Ends user session
+     * @method POST
+     *
+     */
+    public function actionLogout() 
+    {
+        $headers = Yii::$app->request->getHeaders();
+        $headers = explode(" ", $headers['authorization']);
+
+        $accessToken = AccessToken::find()->where(['access_token' => $headers[1]])->one();
+        $accessToken->token_valid = false;
+        $accessToken->save();
+
+        $this->_response = [
+            'success' => true,
+            'desc' => 'Berhasil keluar',
+            'data' => null
+        ];
+        $this->_code = 200;
+
+        return $this->_sendResponse($this->_response, $this->_code);
+    }
+
+    private function _sendResponse($data, $status_code)
+    {
+        return Yii::createObject([
+            'class' => 'yii\web\Response',
+            'format' => \yii\web\Response::FORMAT_JSON,
+            'data' => $data,
+            'statusCode' => $status_code
+        ]);  
     }
 }
---
+++ models/Forms/EditAccountForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models\Forms;
+
+use Yii;
+use yii\base\Model;
+
+use app\models\User;
+
+/**
+ * EditAccountForm is model behind validation of edit account
+ * 
+ * @property User|null $user This prop is read-only
+ * 
+ */
+class EditAccountForm extends Model
+{
+    public $first_name;
+    public $last_name;
+    public $user_name;
+    public $password;
+
+    private $_user = false;
+
+    public function __construct()
+    {
+        $this->_user = User::findOne(Yii::$app->user->id);
+    }
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules() 
+    {
+        return [
+            [['first_name'], 'required'],
+            [['first_name', 'last_name', 'user_name', 'password'], 'string', 'max' => 50],
+            // ['user_name', 'unique']
+        ];
+    }
+
+    /**
+     * 
+     */
+    public function executeEditAccount()
+    {
+        if ($this->validate()) 
+        {
+            $this->_user->first_name = $this->first_name;
+            $this->_user->last_name = $this->last_name;
+            if (!empty($this->user_name)) $this->_user->user_name = $this->user_name;
+            if (!empty($this->password)) $this->_user->makePassword($this->password);
+
+            return $this->_user->save();
+        } else {
+            return false;
+        }
+    }
+
+    public function getUser()
+    {
+        $res = $this->_user;
+        unset($res['password_hashed']);
+
+        return $res;
+    }
+}
