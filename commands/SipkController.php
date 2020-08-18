8f6028de31d9a32d11850f35437d91ccacc233f2
Abdilah Sammi <abdilah@roomme.space>
2020-08-18 10:37:31 +0700

    Add Initialize App Command

--- commands/SipkController.php
+++ commands/SipkController.php
@@ -10,6 +10,9 @@ use app\models\User;
 use yii\console\widgets\Table;
 use yii\helpers\Console;
 
+use app\models\Faculty;
+use app\models\Major;
+
 /**
  * Manage SIPK Environment.
  *
@@ -19,6 +22,36 @@ use yii\helpers\Console;
  */
 class SipkController extends Controller
 {
+    private $permission = [
+        'viewUserManagement',
+        'createUserManagement',
+        'updateUserManagement',
+        'deleteUserManagement',
+    ];
+
+    private $role = [
+        'super-admin',
+        'staff',
+    ];
+
+    private $assign = [
+        'super-admin' => [
+            'viewUserManagement',
+            'createUserManagement',
+            'updateUserManagement',
+            'deleteUserManagement',
+        ],
+        'staff' => [
+            'viewUserManagement'
+        ]
+    ];
+
+    private $faculties = [
+        'Teknik' => [
+            'Informatika S1'
+        ]
+    ];
+
     /**
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
@@ -32,6 +65,70 @@ class SipkController extends Controller
     }
 
     /**
+     * Initialize Application Core Data
+     * @return int Exit code
+     */
+    public function actionInitialize()
+    {
+        foreach($this->faculties as $faculty => $majors) {
+            $fc = new Faculty;
+            $fc->name = $faculty;
+            if($fc->save()) {
+                foreach($majors as $major) {
+                    $mj = new Major;
+                    $mj->name = $major;
+                    $mj->faculty_id = $fc->id;
+                    $mj->save();
+                }
+            }
+        }
+        echo $this->ansiFormat("[Faculty & Major] Success!", Console::FG_GREEN);
+        $this->initializeRoles();
+        return ExitCode::OK;
+    }
+
+    private function initializeRoles()
+    {
+        $auth = Yii::$app->authManager;
+        $auth->removeAll();
+
+        $manageUserMan = $auth->createPermission('manageUserManagement');
+        $manageUserMan->description = "User can access User Management Module";
+        $auth->add($manageUserMan);
+
+        $viewUserMan = $auth->createPermission('viewUserManagement');
+        $viewUserMan->description = "User can view user detail on User Management Module";
+        $auth->add($viewUserMan);
+
+        $createUserMan = $auth->createPermission('createUserManagement');
+        $createUserMan->description = "User can create new user on User Management Module";
+        $auth->add($createUserMan);
+
+        $updateUserMan = $auth->createPermission('updateUserManagement');
+        $updateUserMan->description = "User can update user on User Management Module";
+        $auth->add($updateUserMan);
+
+        $deleteUserMan = $auth->createPermission('deleteUserManagement');
+        $deleteUserMan->description = "User can delete user on User Management Module";
+        $auth->add($deleteUserMan);
+
+        $staffRole = $auth->createRole('staff');
+        $auth->add($staffRole);
+        $auth->addChild($staffRole, $manageUserMan);
+        $auth->addChild($staffRole, $viewUserMan);
+
+        $adminRole = $auth->createRole('super-admin');
+        $auth->add($adminRole);
+        $auth->addChild($adminRole, $createUserMan);
+        $auth->addChild($adminRole, $updateUserMan);
+        $auth->addChild($adminRole, $deleteUserMan);
+        $auth->addChild($adminRole, $staffRole);
+
+        echo "[Roles & Permission] Done!";
+        // return ExitCode::OK;
+    }
+
+    /**
      * Create an Super Administrator Account
      * @param string $username Account username.
      * @param string $email Account email address.
@@ -39,6 +136,7 @@ class SipkController extends Controller
      */
     public function actionCreateSuAccount($username, $email, $password = null)
     {
+        $auth = Yii::$app->authManager;
         $_password = (!empty($password)) ? $password : Yii::$app->getSecurity()->generateRandomString();
 
         $user = new User();
@@ -49,6 +147,7 @@ class SipkController extends Controller
         $user->password_hashed = Yii::$app->getSecurity()->generatePasswordHash($_password);
         $user->account_status = User::ACCOUNT_ACTIVE;
         if ($user->save()) {
+            $auth->assign($auth->getRole('super-admin'), $user->id);
             echo "Account created successfully\n";
             echo Table::widget([
                 'headers' => ['Details'],
